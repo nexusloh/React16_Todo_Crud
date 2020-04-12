eaca3c7d0b3d2a1ac11fb541f067ee43dd544b80
dmitrosh <dmytrosh@default-value.com>
2020-04-12 20:29:37 +0300

    Added strikethrough functionality

--- src/components/TodoListItem/TodoListItem.js
+++ src/components/TodoListItem/TodoListItem.js
@@ -2,8 +2,27 @@ import React, { Component } from 'react';
 import './TodoListItem.scss';
 
 export default class TodoListItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            done: false
+        };
+
+        this.oneClick = () => {
+            this.setState({
+                done: true
+            });
+        };
+    }
+
     render() {
         const { label, important = false } = this.props;
+
+        const { done } = this.state;
+        let classNames = '';
+        if (done) {
+            classNames += ' done'
+        }
 
         const style = {
             color: important ? 'blue' : 'black'
@@ -12,10 +31,10 @@ export default class TodoListItem extends Component {
         return (
             <div className={'d-flex justify-content-between align-items-center'}>
                 <span>
-                    <span style={style}>{ label }</span>
+                    <span className={classNames} onClick={this.oneClick} style={style}>{ label }</span>
                 </span>
 
-                    <span>
+                <span>
                     <span className={'badge badge-danger mr-2'}>x</span>
                     <span className={'badge badge-primary badge-pill'}>i</span>
                 </span>
